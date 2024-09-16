ebfd5932deaa0b62b3c0b1a421270e119fd18eb7
Jorge Castillo <jj.jorgecastillo@gmail.com>
2024-09-16 16:14:24 -0500

    test vercel

--- config/database.ts
+++ config/database.ts
@@ -2,18 +2,30 @@ export default ({ env }) => {
   const client = env("DATABASE_CLIENT", "postgres");
 
   const connections = {
-    postgres: {
-      connection: {
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "1234"),
+    // postgres: {
+    //   connection: {
+    //     host: env("DATABASE_HOST", "localhost"),
+    //     port: env.int("DATABASE_PORT", 5432),
+    //     database: env("POSTGRES_DATABASE", "strapi"),
+    //     user: env("DATABASE_USERNAME", "postgres"),
+    //     password: env("DATABASE_PASSWORD", "1234"),
+    //   },
+    //   pool: {
+    //     min: env.int("DATABASE_POOL_MIN", 2),
+    //     max: env.int("DATABASE_POOL_MAX", 10),
+    //   },
+    // },
+    client: "postgres",
+    connection: {
+      host: env("POSTGRES_HOST"),
+      port: env.int("POSTGRES_PORT"),
+      database: env("POSTGRES_DATABASE"),
+      user: env("POSTGRES_USERNAME"),
+      password: env("POSTGRES_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: true, // Asegúrate de permitir SSL sin verificar el certificado
       },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
+      debug: false,
     },
   };
 
